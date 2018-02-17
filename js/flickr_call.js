a5783e47a515f96097579fa3212e0d82fafebdc2
tuckpuck <tucker.triggs@yahoo.com>
2018-02-17 09:03:43 +0700

    getting info from new api request

--- js/flickr_call.js
+++ js/flickr_call.js
@@ -1,37 +1,47 @@
-var myUrl = 'https://api.flickr.com/services/feeds/photos_public.gne?id=147634988@N06&format=json&nojsoncallback=1'
+// var myUrl = 'https://api.flickr.com/services/feeds/photos_public.gne?id=147634988@N06&format=json&nojsoncallback=1'
+var myUrl = 'https://api.flickr.com/services/rest/?api_key=a74d8c55e6c6a6af7628ac7825dfacde&method=flickr.people.getPhotos&user_id=147634988@N06&format=json&per_page=500&content_type=1&extras=url_z,url_q,description'
 var proxy = 'https://cors-anywhere.herokuapp.com/';
 var finalURL = proxy + myUrl;
 
 $.ajax({
-    url: finalURL,
-    success: function(data) {
-        console.log(data);
-        addImages(data);
-    }
+  beforeSend: function(request) {
+    request.setRequestHeader('X-Requested-With','XMLHttpRequest');
+  },
+  url: finalURL,
+  success: function(data) {
+    console.log(data);
+    addImages(data);
+  }
 });
 
 /*
 Get your Flickr Id from: http://idgettr.com/ and replace it with the flickrId variable on the next line. That way you have access to the images that are associated with your account.
 */
-// https://api.flickr.com/services/rest/";var flickrOptions={method:"flickr.photos.search",user_id:"136839724@N02",api_key:"e94ed6119afae842434d2aaf7e636558"
-//     privacy_filter:1,tags:$jscomp$loop$1.country,extras:"url_m,url_z",per_page:100,format:"json",nojsoncallback:1}
+
+
 
 /*-------------------------------------
                   FEEDS
 ---------------------------------------*/
 /********* PERSONAL *********/
-var flickrURL = 'https://api.flickr.com/services/feeds/photos_public.gne?id=147634988@N06&format=json&nojsoncallback=1';
 
-
-
-
-/*-------------------------------------
-            FLICKR API CALL
----------------------------------------*/
-// Using jQuery we can easily make the call to flickr using $.getJSON
-// $.getJSON(flickrURL).done(function(data) {
-  // console.log(data);
-  // addImages(data);
+// $.ajaxSetup({
+//   headers: {
+//     'x-requested-with':'XMLHttpRequest',
+//     'Access-Control-Allow-Origin':'*'
+//   }
+// });
+//
+// var flickrURL = 'https://api.flickr.com/services/rest/?api_key=a74d8c55e6c6a6af7628ac7825dfacde&method=flickr.people.getPhotos&user_id=147634988@N06&format=json&per_page=500&content_type=1&extras=url_z,url_q,description';
+// var proxy = 'https://cors-anywhere.herokuapp.com/';
+// var finalURL = proxy + flickrURL;
+// console.log(finalURL);
+// //
+// //
+// //
+// $.getJSON(finalURL,function(data){
+//   console.log(data);
+//   addImages(data);
 // });
 
 /*-------------------------------------
@@ -50,5 +60,6 @@ function addImages(data) {
     flickrItem.appendChild(flickrItem.img);
 
     $('.gallery').append(flickrItem);
+    console.log(flickrItem);
   });
 }
